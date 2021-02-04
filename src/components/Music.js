ff9fe9780d639ccc08ae5aee98672d1d2683666a
Seann Branchfield <eselbeus@gmail.com>
2021-02-04 12:32:14 -0500

    Add music and update font

--- src/components/Music.js
+++ src/components/Music.js
@@ -2,8 +2,13 @@ import '../App.css';
 
 function Music(){
   return(
-    <div>
-      <h1>Music</h1>
+    <div className="music-page">
+      <p>P.Lucky on iTunes/Apple Music</p>
+      <iframe allow="autoplay *; encrypted-media *; fullscreen *" frameborder="0" height="450" sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation" src="https://embed.music.apple.com/us/album/im-stuck-single/1552233617"></iframe>
+      <p>P.Lucky on Spotify</p>
+      <iframe src="https://open.spotify.com/embed/artist/2LBJr3uzzw5jg2ngbh2PpC" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+      <p>P.Lucky Kobby A. on Spotify</p>
+      <iframe src="https://open.spotify.com/embed/artist/36zFDKB2TW3pwkeemBqplq" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
     </div>
   )
 }
