db6ca2d7abc496f87bc600c2b10a30a2cda50a28
Seann Branchfield <eselbeus@gmail.com>
2021-02-13 01:03:42 -0500

    Revise icons, fonts, and text

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,22 +1,14 @@
 import '../App.css';
-import ig from '../assets/ig-icon-white.webp'
-import yt from '../assets/yt-icon-white.webp'
-import sp from '../assets/sp-icon-white.webp'
-import it from '../assets/itunes-icon-white.webp'
-import sc from '../assets/soundcloud-icon-white.webp'
+import paypal from '../assets/paypal-icon-w.png'
 
 function Footer(){
   return(
     <div className="footer">
       <h1>Contact</h1>
-      <p>For booking contact: <a href="mailto:p.luckyinfo@gmail.com">p.luckyinfo@gmail.com</a></p>
-      <span className="social-icons">
-        <a href="https://www.instagram.com/p.lucky__official"><img src={ig} alt="instagram"/></a>
-        <a href="https://www.youtube.com/channel/UCee6mxIZsuwynoxdpuZa8zg"><img src={yt} alt="youtube"/></a>
-        <a href="https://open.spotify.com/artist/2LBJr3uzzw5jg2ngbh2PpC"><img src={sp} alt="spotify"/></a>
-        <a href="https://music.apple.com/us/artist/p-lucky/1512324079"><img src={it} alt="apple music"/></a>
-        <a href="https://soundcloud.com/user-771075733"><img src={sc} alt="soundcloud"/></a>
-      </span>
+      <p>For booking and information email <a href="mailto:p.luckyinfo@gmail.com">p.luckyinfo@gmail.com</a></p>
+      <p></p>
+      <p>Thank you for helping to support my music projects in 2021 and beyond!</p>
+      <a href="https://www.paypal.com/paypalme/pluckyprojects"><img className="social-icon" src={paypal}/></a>
       <p></p>
       <a href='http://www.freevisitorcounters.com' className="counter-text">freecounter</a> <script type='text/javascript' src='https://www.freevisitorcounters.com/auth.php?id=7a38c9e853c4900287d6182e453819c4e6933356'></script>
       <script type="text/javascript" src="https://www.freevisitorcounters.com/en/home/counter/786538/t/5"></script>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import logo from '../assets/Logo.jpg'
 import vid1 from '../assets/VID-1.mp4'
+import ig from '../assets/ig-icon.png'
+import yt from '../assets/yt-icon.png'
+import sp from '../assets/spotify-icon.png'
+import it from '../assets/itunes-icon-white.webp'
+import sc from '../assets/soundcloud-icon-t.png'
 import '../App.css';
 
 function Home(){
@@ -8,6 +13,14 @@ function Home(){
     <div className='home'>
       <img className="logo" alt="logo" src={logo} />
       <div className="bio">
+      <p>Follow on</p>
+      <span className="social-icons">
+        <a href="https://www.instagram.com/p.lucky__official"><img className="social-icon" src={ig} alt="instagram"/></a>
+        <a href="https://www.youtube.com/channel/UCee6mxIZsuwynoxdpuZa8zg"><img className="social-icon" src={yt} alt="youtube"/></a>
+        <a href="https://open.spotify.com/artist/2LBJr3uzzw5jg2ngbh2PpC"><img className="social-icon" src={sp} alt="spotify"/></a>
+        <a href="https://music.apple.com/us/artist/p-lucky/1512324079"><img className="social-icon" src={it} alt="apple music"/></a>
+        <a href="https://soundcloud.com/user-771075733"><img className="social-icon" src={sc} alt="soundcloud"/></a>
+      </span>
       <p>P. Lucky is a rapper and songwriter from Vienna, Austria. </p>
       <p>"I began rapping at the age of 31 and I wrote and recorded my first song in April, 2020. One of my favorite artists/musical influences is Travis Garland. In my free time I enjoy movies, hanging out with friends and going to the gym.
 
